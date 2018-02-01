ecb146689ced0431f9fa1fb935f9436841c0755b
Vytautas Stankus <svycka@users.noreply.github.com>
2018-02-01 08:48:24 +0200

    fixes auto-completion for IDE

--- src/shim.php
+++ src/shim.php
@@ -1,6 +1,6 @@
 <?php
 if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit'.'\Framework\TestCase');
 }
 
 if (!class_exists('PHPUnit\Runner\Version')) {
