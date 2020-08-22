23614d8f9faff13f698982033f4a99e7fecd8dae
Lucas Aerbeydt <lucas@ectoma.com>
2020-08-22 15:56:15 +0200

    Add PHPDocs

--- src/Stub/StubMarshaler.php
+++ src/Stub/StubMarshaler.php
@@ -31,11 +31,34 @@ class StubMarshaler
         return $this->methodValue;
     }
 
+    /**
+     * @return array|null expected method arguments, null makes it skip the check
+     */
     public function getArguments()
     {
         return $this->methodArguments;
     }
 
+    /**
+     * Checks if a method is called with the specified arguments
+     *
+     * If the method is invoked with different arguments, an exception will be thrown.
+     *
+     * ```php
+     * <?php
+     * use \Codeception\Stub\Expected;
+     *
+     * $user = $this->make('User', [
+     *      'setName' => Expected::once()->with('Davert')
+     * ]);
+     * $user->setName('Davert');
+     * ?>
+     * ```
+     *
+     * @see \PHPUnit\Framework\MockObject\Builder\InvocationMocker::with
+     * @param array ...$arguments
+     * @return StubMarshaler
+     */
     public function with(...$arguments)
     {
         $this->methodArguments = $arguments;
